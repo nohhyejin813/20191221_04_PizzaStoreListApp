6d23b7a92349b6bb0f2b97534433aa1bd806948a
nohhyejin813 <nohhyejin813@gmai.com>
2019-12-21 15:35:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a20191221_04_pizzastorelistapp/datas/PizzaStore.kt
@@ -0,0 +1,10 @@
+package com.example.a20191221_04_pizzastorelistapp.datas
+
+class PizzaStore(storeName:String, phoneNum:String, logoUrl:String) {
+
+    var storeName = storeName
+    var phoneNum = phoneNum
+    var logoUrl = logoUrl
+
+    
+}
